45e6888f48f33f5d6bc50e87261607ce0097c658
Arturo Eyck Tapia Ramos <arturo@Arturos-MacBook-Air.local>
2024-10-09 12:48:49 -0500

    initial commit

---
+++ TimR/TimRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimRApp.swift
+//  TimR
+//
+//  Created by Arturo Eyck Tapia Ramos on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TimRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimRTests/TimRTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimRTests.swift
+//  TimRTests
+//
+//  Created by Arturo Eyck Tapia Ramos on 9/10/24.
+//
+
+import Testing
+@testable import TimR
+
+struct TimRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
