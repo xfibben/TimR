10079e1ae5600b03aace8a37fc7b20ccca54f27d
Arturo Eyck Tapia Ramos <arturo@Arturos-MacBook-Air.local>
2024-10-09 16:36:20 -0500

    initialize

--- TimR/ContentView.swift
+++ TimR/ContentView.swift
@@ -4,19 +4,41 @@
 //
 //  Created by Arturo Eyck Tapia Ramos on 9/10/24.
 //
-
 import SwiftUI
 
-var time = 0
+class Times: ObservableObject {
+    @Published var time: TimeInterval = 0.0
+    var timer: Timer?
+    var startDate: Date?
+    
+    func startTime() {
+        startDate = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) { _ in
+            if let startDate = self.startDate {
+                self.time = Date().timeIntervalSince(startDate)
+            }
+        }
+    }
+    
+    func stopTime() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
 
 struct ContentView: View {
+    @StateObject var times = Times()
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
             Button("Start") {
-                time = 20
+                times.startTime()
             }
-            Text("\(time)")
+            Button("Stop") {
+                times.stopTime()
+            }
+            Text("\(times.time, specifier: "%.3f") seconds")
         }
         .padding()
     }
@@ -25,3 +47,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
